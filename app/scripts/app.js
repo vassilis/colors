ec1a5111c2d3a9ef80f34abe6b4c33e5a8d0e97e
vassilis <vas.terzo@gmail.com>
2013-10-30 00:30:26 +0200

    added index with links

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,17 +1,41 @@
 'use strict';
 
-angular.module('colourloversClientApp', [
-  'ngCookies',
-  'ngResource',
-  'ngSanitize'
+window.App = angular.module('colourloversClientApp', [
+	'ngCookies',
+	'ngResource',
+	'ngSanitize'
 ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+
+App.config(function ($routeProvider) {
+	$routeProvider
+		.when('/', {
+			templateUrl: 'views/main.html',
+			controller: 'App.Controllers.MainCtrl'
+		})
+		.when('/palettes/top', {
+			templateUrl: 'views/palettes.html',
+			controller: 'App.Controllers.PalettesTopCtrl'
+		})
+		.when('/palettes/new', {
+			templateUrl: 'views/palettes.html',
+			controller: 'App.Controllers.PalettesNewCtrl'
+		})
+		.when('/palettes', {
+			templateUrl: 'views/palettes.html',
+			controller: 'App.Controllers.PalettesCtrl'
+		})
+		.otherwise({
+			redirectTo: '/'
+		});
+});
+
+// Enable HTML5 history so we can have URLs without hash 
+// App.config(function ($locationProvider) {
+// 	$locationProvider.html5Mode(true);
+// })
+
+// Controllers Namespace
+App.Controllers = {}
+
+// App data namespace
+App.Data = {}
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,56 +1,8 @@
 'use strict';
 
-angular.module('colourloversClientApp')
-	.controller('MainCtrl', function ($scope, $http) {
-
-		$scope.active = 0;
-
-		$http.jsonp('http://www.colourlovers.com/api/palettes&jsonCallback=JSON_CALLBACK&numResults=100')
-		.success(function(data, status, headers, config){
-			// console.log(data, status, headers, config);
-			$scope.palettes = data;
-		})
-		.error(function(data, status, headers, config){
-			$scope.errors = "Something went wrong";
-		})
-
-	})
-	.directive('color', function(){
-		return {
-			link: function(scope, el, attrs) {
-				var $el = $(el);
-				$el.on('click', function(){
-					selectText(this.getElementsByTagName("span")[0]);
-					$('.color').not(this).removeClass('active');
-					$(this).toggleClass('active');
-				})
-			}
-		}
-	})
-	.directive('light', function(){
-		return {
-			link: function(scope, el, attrs) {
-				var $el = $(el);
-				$el.on('click', function(){
-					$('body').toggleClass('light');
-				})
-			}
-		}
-	})
+App.Controllers.MainCtrl = function ($scope, $http) {
 
 
-function selectText(el) {
-		var doc = document;
 
-		if (doc.body.createTextRange) { // ms
-				var range = doc.body.createTextRange();
-				range.moveToElementText(el);
-				range.select();
-		} else if (window.getSelection) { // moz, opera, webkit
-				var selection = window.getSelection();
-				var range = doc.createRange();
-				range.selectNodeContents(el);
-				selection.removeAllRanges();
-				selection.addRange(range);
-		}
+
 }
